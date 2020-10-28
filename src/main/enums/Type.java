954ac189f185ab0e56331d4a720f034fefc97be9
Giovani Acosta <giovaniacosta.721@gmail.com>
2020-10-28 19:14:37 -0400

    Adding all move data and type advantage multipliers as text. To be parsed into usable objects and data.

--- src/main/enums/Type.java
+++ src/main/enums/Type.java
@@ -1,7 +1,29 @@
 package main.enums;
 
+/**
+ * Enum to define the types that creatures or moves may be.
+ */
+
 public enum Type {
 
-    Water, Fire, Grass;
+    Bug,
+    Dark,
+    Dragon,
+    Electric,
+    Fairy,
+    Fighting,
+    Fire,
+    Flying,
+    Ghost,
+    Grass,
+    Ground,
+    Ice,
+    Normal,
+    Poison,
+    Psychic,
+    Rock,
+    Steel,
+    Water
+
 
 }
